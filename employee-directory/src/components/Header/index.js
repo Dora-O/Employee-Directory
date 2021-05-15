7915c307c4176dcfd4bb26610abc40d201109f0b
Dora O <dorozco2390@utexas.edu>
2021-05-14 22:53:17 -0500

    added header index file

---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+  return (
+  <header className="bg-dark text-white p-3 text-center">
+    <h1 className="p-2 m-2">Employee Directory</h1>
+  </header>
+  )
+}
+
+export default Header;
