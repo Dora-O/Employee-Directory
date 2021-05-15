47b26d99f6079a3d83b14873e539ac1dc430b75b
Dora O <dorozco2390@utexas.edu>
2021-05-15 16:46:35 -0500

    added p tag letting user know which columns can sort employees

--- employee-directory/src/components/Header/index.js
+++ employee-directory/src/components/Header/index.js
@@ -4,6 +4,7 @@ function Header() {
   return (
   <header className="bg-dark text-white p-3 text-center">
     <h1 className="p-2 m-2">Employee Directory</h1>
+    <p>Click on "Phone Number" to sort numerically or "Email" to sort alphabetically.</p>
   </header>
   )
 }
